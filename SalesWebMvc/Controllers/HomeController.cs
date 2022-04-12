9378bb6621d7a4604511f19b2158105fc2eab260
stolpe22 <57258862+stolpe22@users.noreply.github.com>
2022-04-11 23:57:47 -0400

    Alterações feitas
    Razor @ para blocos c# dentro do cshtml

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,8 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-            ViewData["email"] = "stolpe02@gmail.com";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["Author"] = "Lucas Stolpe";
 
             return View();
         }
